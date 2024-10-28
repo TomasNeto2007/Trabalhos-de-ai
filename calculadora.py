4f8e13557209715c24546fde30323db3e5ab4440
TomasNeto2007 <tomasmlsneto@gmail.com>
2024-10-28 13:21:31 +0000

    Operações da calculadora

---
+++ calculadora.py
@@ -0,0 +1,15 @@
+#Função de cada operação
+def soma(x, y):
+     return x + y
+
+def subtracao(x, y):
+    return x - y
+
+def multiplicacao(x, y):
+    return x * y
+
+def divisao(x, y):
+   if y !=0:
+    return x / y
+   else:
+       return "Erro: Divisão por zero não é permitida"
