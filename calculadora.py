8d59ac817c933302bbd8f314ff16c6368019133d
TomasNeto2007 <tomasmlsneto@gmail.com>
2024-10-28 18:15:28 +0000

    Historico

--- calculadora.py
+++ calculadora.py
@@ -13,3 +13,18 @@ def divisao(x, y):
     return x / y
    else:
        return "Erro: Divisão por zero não é permitida"
+        
+# Função para armazenar as últimas 5 operações
+def armazenar_operacao(operacao, historico):
+    if len(historico)>=5:
+        historico.pop(0)
+    historico.append(operacao)  # Adiciona a nova operação
+
+# Função para exibir o histórico
+def exibir_historico(historico):
+    if len(historico) == 0:
+        print("Nenhuma operação armazenada.")
+    else:
+        print("\nHistórico das últimas 5 operações:")
+        for i, op in enumerate(historico,1):
+            print(f"{i}.{op}")
